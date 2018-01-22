71ab06096d742aed6ade9874eb91c8c564d19f6a
IPeter <illes.peter.deb@gmail.com>
2018-01-22 16:52:51 +0100

    progress

--- app/src/main/java/com/peterilles/dartscounter/dartscounter/GameActivity.java
+++ app/src/main/java/com/peterilles/dartscounter/dartscounter/GameActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 
 public class GameActivity extends Activity {
 
+    protected int score = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        score = getIntent().getIntExtra("game", 501);
     }
 
     @Override
